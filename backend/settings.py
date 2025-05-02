d108c17d3feddb93708acd3c044c71b1c449a053
Ibarahona73 <igyt2015@gmail.com>
2025-05-02 11:42:18 -0500

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -142,7 +142,9 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+REST_FRAMEWORK = {
 
+ 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 
-
+}
 
--- backend/urls.py
+++ backend/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework.documentation import include_docs_urls
 
 #aqui se importan las rutas de la api creadas en urls.py de la app tareas
 urlpatterns = [    
     path('admin/', admin.site.urls),
     path('api/v1/', include('tareas.urls')),
-]
+    path('docs/', include_docs_urls(title="Informacion Api"))]
